933fdbd7501aa13cdde7b59118b7737a3c0e1787
woojin-kang <woojin.kang@toss.im>
2021-11-28 12:13:59 +0900

    Add init

---
+++ gofdesignpattern/src/main/kotlin/com/example/gofdesignpattern/GofdesignpatternApplication.kt
@@ -0,0 +1,11 @@
+package com.example.gofdesignpattern
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GofdesignpatternApplication
+
+fun main(args: Array<String>) {
+    runApplication<GofdesignpatternApplication>(*args)
+}
