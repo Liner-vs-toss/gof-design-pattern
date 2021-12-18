a3f10711f75e49539d132bf91beca78cf460550b
woojin-kang <woojin.kang@toss.im>
2021-12-18 17:48:52 +0900

    week04 과제 추가

--- gofdesignpattern/src/test/kotlin/com/example/gofdesignpattern/week04_builder/Transfer.kt
+++ gofdesignpattern/src/test/kotlin/com/example/gofdesignpattern/week04_builder/Transfer.kt
@@ -3,7 +3,7 @@ package com.example.gofdesignpattern.week04_builder
 interface Transfer {
     val fromBankCode: Int
     val toBankCode: Int
-    val amount: Int
+    val amount: Long
     val summary: String?
     val name: String
 }
--- gofdesignpattern/src/test/kotlin/com/example/gofdesignpattern/week04_builder/TransferBuilderTest.kt
+++ gofdesignpattern/src/test/kotlin/com/example/gofdesignpattern/week04_builder/TransferBuilderTest.kt
@@ -3,6 +3,7 @@ package com.example.gofdesignpattern.week04_builder
 import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeSameInstanceAs
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class TransferBuilderTest {
@@ -10,22 +11,33 @@ class TransferBuilderTest {
     @Test
     fun `toBankCode가 92면 TossBankTransfer가 생성된다`() {
         val transfer = DefaultTransferBuilder()
+            .bankCode(10, 92)
+            .name("강우진")
+            .amount(1)
+            .summary("테스트")
             .createTransfer()
 
-        transfer shouldBeSameInstanceAs TossBankTransfer::class
+        assertThat(transfer).isInstanceOf(TossBankTransfer::class.java)
     }
 
     @Test
     fun `toBankCode가 92가 아니면 AnotherBankTransfer가 생성된다`() {
         val transfer = DefaultTransferBuilder()
+            .bankCode(10, 88)
+            .name("강우진")
+            .amount(1)
+            .summary("테스트")
             .createTransfer()
 
-        transfer shouldBeSameInstanceAs AnotherBankTransfer::class
+        assertThat(transfer).isInstanceOf(AnotherBankTransfer::class.java)
     }
 
     @Test
     fun `summary를 입력안한 경우 summary에 name이 적용된다`() {
         val transfer = DefaultTransferBuilder()
+            .bankCode(10, 92)
+            .name("강우진")
+            .amount(1)
             .createTransfer()
 
         transfer.summary shouldBe transfer.name
@@ -35,6 +47,8 @@ class TransferBuilderTest {
     fun `amount가 0보다 작은 경우 RuntimeException이 발생한다`() {
         shouldThrowAny {
             DefaultTransferBuilder()
+                .bankCode(10, 92)
+                .name("강우진")
                 .amount(-1)
                 .createTransfer()
         }
@@ -45,6 +59,8 @@ class TransferBuilderTest {
         shouldThrowAny {
             DefaultTransferBuilder()
                 .name("가나다라마바사아차카타파하")
+                .bankCode(10, 92)
+                .amount(4)
                 .createTransfer()
         }
     }
